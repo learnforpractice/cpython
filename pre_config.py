c753750077ca4a80691a813427054703b05a9fc5
learnforpractice <learnforpractice@gmail.com>
2018-10-01 19:50:18 +0800

    Fix build on Centos

---
+++ pre_config.py
@@ -0,0 +1,4 @@
+import sys
+with open('Modules/Setup.local', 'w') as fout:
+        fout.write("SSL=%s\n"%(sys.argv[1],))
+        fout.write("_ssl _ssl.c -DUSE_SSL -I$(SSL)/include -I$(SSL)/include/openssl -L$(SSL)/lib -lssl -lcrypto\n")
